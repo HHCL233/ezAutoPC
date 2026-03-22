34f7370d4309ff5f117ec71e032acea7b8f8d2f8
HHCL233 <XINGTAISHIJIAOXIQU@outlook.com>
2026-03-22 23:09:48 +0800

    feat: 给WebUI中WebUI设置添加了保存按钮

--- dashboard/src/router/Setting/Webui.vue
+++ dashboard/src/router/Setting/Webui.vue
@@ -6,6 +6,7 @@
             <mdui-list-item :headline="`选项${index}`" :description="`打开选项${index}后的操作`" v-for="index in 0"><mdui-checkbox
                     slot="end-icon"></mdui-checkbox></mdui-list-item>
         </mdui-list>
+        <mdui-fab icon="save" class="setting-webui-save" @click=""></mdui-fab>
     </div>
 </template>
 <style scoped>
@@ -15,5 +16,11 @@
 
 .setting-webui {
     margin: 16px;
+}
+
+.setting-webui-save {
+    position: fixed;
+    right: 36px;
+    bottom: 36px;
 }
 </style>
