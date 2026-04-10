76434ec468b6eda504f882542059d3939ad7c63e
HHCL233 <XINGTAISHIJIAOXIQU@outlook.com>
2026-04-10 20:30:11 +0800

    feat: 插件格式更改,支持自动根据requirements.txt安装插件依赖

--- autopc/plugins.py
+++ autopc/plugins.py
@@ -2,6 +2,7 @@ import sys
 import os
 from pathlib import Path
 import importlib.util
+import subprocess
 
 
 class PluginsManager:
@@ -19,27 +20,51 @@ class PluginsManager:
         print("[插件管理] 加载插件中...")
         self.plugins_dir.mkdir(exist_ok=True)
 
-        for file in os.listdir(self.plugins_dir):
-            print(file)
-            if not file.endswith(".py"):
+        for item in os.listdir(self.plugins_dir):
+            plugin_folder = self.plugins_dir / item
+            if not plugin_folder.is_dir():
                 continue
-            if file.startswith("_"):
+            main_file = plugin_folder / "main.py"
+            requirements_file = plugin_folder / "requirements.txt"
+            if not main_file.exists():
                 continue
 
-            plugin_path = self.plugins_dir / file
+            # 自动安装依赖
+            if requirements_file.exists():
+                # 依赖安装
+                print(f"[插件管理] 检测到 {item} 存在依赖文件")
+                is_install_requirements = input(
+                    f"[插件管理] 是否在当前环境安装 {item} 的依赖? (y/N)"
+                )
+                if is_install_requirements.lower() != "y":
+                    print(f"[插件管理] 拒绝安装 {item} 的依赖")
+                    print(f"[插件管理] {item} 停止导入")
+                    continue
+                print(f"[插件管理] 安装 {item} 依赖中...")
+                install_cmd = [
+                    sys.executable,
+                    "-m",
+                    "pip",
+                    "install",
+                    "-r",
+                    str(requirements_file),
+                ]
+                try:
+                    subprocess.check_call(install_cmd)
+                    print(f"[插件管理] {item} 的依赖安装成功")
+                except subprocess.CalledProcessError as e:
+                    print(f"[插件管理] {item} 的依赖安装失败: {e}")
+                    print(f"[插件管理] {item} 停止导入")
+                    continue
 
-            spec = importlib.util.spec_from_file_location(
-                f"plugin.{file[:-3]}", plugin_path
-            )
-
+            spec = importlib.util.spec_from_file_location(f"plugin.{item}", main_file)
             if spec is None or spec.loader is None:
-                print(f"[插件管理] 插件 {file} 加载失败：无效模块")
+                print(f"[插件管理] 插件 {item} 加载失败：无效模块")
                 continue
 
             plugin = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(plugin)
-
-            print(f"[插件管理] 加载插件成功: {file}")
+            print(f"[插件管理] 加载插件成功: {item}")
         from autopc.api.event import register_all_commands
 
         register_all_commands(self.autopc)
---
+++ plugins/test/main.py
@@ -0,0 +1,10 @@
+from autopc.api.event import registr_command
+import requests
+
+
+# 指令示例
+@registr_command("测试指令")
+def test_command(command, url):
+    print("你输入了url:", url)
+    content = requests.get(url)
+    return "网站内容:" + content.text
