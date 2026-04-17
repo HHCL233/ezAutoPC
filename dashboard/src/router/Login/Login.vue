c3819459abee358379d9cea693af3dce101e483c
HHCL233 <XINGTAISHIJIAOXIQU@outlook.com>
2026-04-17 20:49:27 +0800

    fix: 密码框类型不正确

--- dashboard/src/router/Login/Login.vue
+++ dashboard/src/router/Login/Login.vue
@@ -3,7 +3,6 @@ import { useConfigStore } from '@/stores/config';
 import { ref } from 'vue';
 import type { TextField } from 'mdui/components/text-field.js';
 import { generalFetch } from '@/utils/fetch';
-import { setSecureCookie } from '@/utils/cookie';
 import { dialog } from "mdui/functions/dialog.js";
 
 const config = useConfigStore()
@@ -53,7 +52,8 @@ const login = async () => {
     <div class="login">
         <h1 class="login-title">登录 ezAutoPC</h1>
         <mdui-text-field variant="outlined" label="账号" class="login-text-field" ref="loginAccount"></mdui-text-field>
-        <mdui-text-field variant="outlined" label="密码" class="login-text-field" ref="loginPassword"></mdui-text-field>
+        <mdui-text-field variant="outlined" label="密码" type="password" toggle-password class="login-text-field"
+            ref="loginPassword"></mdui-text-field>
         <mdui-button class="login-button" @click="login()">登录</mdui-button>
     </div>
 </template>
